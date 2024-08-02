38ca06f8040a7f60489f3063f181b2e6d668247d
DeraE2024 <careers.joseph@yahoo.com>
2024-08-02 20:06:13 +0100

    add dummy controllers and endpoints

---
+++ user-management.api/Controllers/ApiBaseController.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace user_management.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiBaseController : ControllerBase
+    {
+        private IMediator _mediator;
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+    }
+}
---
+++ user-management.api/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace user_management.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ApiBaseController
+    {
+        private readonly IMapper _mapper;
+        public UsersController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        [HttpGet("id")]
+        public async Task<IActionResult> FetchUserById([FromRoute] string id)
+        {
+            var response = await Mediator.Send(new object());
+            return Ok(response);
+        }
+
+        [HttpGet("tenantId")]
+        public async Task<IActionResult> FetchUsersByTenantId([FromRoute] string tenantId)
+        {
+            var response = await Mediator.Send(new object());
+            return Ok(response);
+        }
+
+        [HttpGet("searchParameter")]
+        public async Task<IActionResult> FetchUserBySearchParameter([FromRoute] string searchParameter)
+        {
+            var response = await Mediator.Send(new object());
+            return Ok(response);
+        }
+    }
+}
