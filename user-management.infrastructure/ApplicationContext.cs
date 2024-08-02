6f8abf2634412c9a44c5d404290d42a18282c5ac
DeraE2024 <careers.joseph@yahoo.com>
2024-08-02 11:12:42 +0100

    configure identity

---
+++ user-management.infrastructure/ApplicationContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using user_management.domain.Entities;
+
+namespace user_management.infrastructure
+{
+    public class ApplicationContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+    }
+}
