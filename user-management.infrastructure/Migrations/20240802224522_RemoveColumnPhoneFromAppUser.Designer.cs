4bc12514266d44b5e6f99280cd6ce41891c080c4
DeraE2024 <careers.joseph@yahoo.com>
2024-08-02 23:50:16 +0100

    add migration

--- user-management.infrastructure/Migrations/20240802224522_RemoveColumnPhoneFromAppUser.Designer.cs
+++ user-management.infrastructure/Migrations/20240802224522_RemoveColumnPhoneFromAppUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using user_management.infrastructure;
 
@@ -11,9 +12,11 @@ using user_management.infrastructure;
 namespace user_management.infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20240802224522_RemoveColumnPhoneFromAppUser")]
+    partial class RemoveColumnPhoneFromAppUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
