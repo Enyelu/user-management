48233e702b51ccddcd2bca6b14244dda48be02e5
DeraE2024 <careers.joseph@yahoo.com>
2024-08-03 13:50:50 +0100

    add logic to fetch tenant staff members

---
+++ user-management.core/DataTransferObjects/StaffDto.cs
@@ -0,0 +1,13 @@
+﻿namespace user_management.core.DataTransferObjects
+{
+    public class StaffDto
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string Avatar { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
