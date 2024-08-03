47301a641ee35ca2f32fd00afd6ca373f6abd951
DeraE2024 <careers.joseph@yahoo.com>
2024-08-03 06:48:27 +0100

    implement add lock to tenant logic

--- user-management.api/Controllers/TenantsController.cs
+++ user-management.api/Controllers/TenantsController.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using user_management.core.Commands.Tenant;
+using user_management.core.DataTransferObjects;
 
 namespace user_management.api.Controllers
 {
@@ -21,9 +23,24 @@ namespace user_management.api.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateTenent([FromBody] object request)
+        public async Task<IActionResult> CreateTenent([FromBody] CreateTenantDto request)
         {
-            var response = await Mediator.Send(new object());
+            var response = await Mediator.Send(new HandleCreateTenant.Command 
+            { 
+                Email = request.Email, 
+                Name = request.Name
+            });
+            return Ok(response);
+        }
+
+        [HttpPatch("add-lock")]
+        public async Task<IActionResult> AddLockToTenant([FromBody] AddLockToTenantDto request)
+        {
+            var response = await Mediator.Send(new HandleAddLockToTenant.Command 
+            { 
+                LockId = request.LockId, 
+                TenantId = request.TenantId
+            });
             return Ok(response);
         }
     }
--- user-management.core/Shared/ApplicationConstants.cs
+++ user-management.core/Shared/ApplicationConstants.cs
@@ -7,5 +7,7 @@ namespace user_management.core.Shared
     {
         public static string EmailConfirmationMsg = $"<p> Dear FirstName \n Confirmation of your email is one click away <a href='callbackUrl'>click here</a> to continue</P>";
         public static string ResetPasswordMsg = $"<p> Dear FirstName, to reset your password, <a href='url'>click here</a></p>";
+        public static string OnboardTenantMsg = $"<p> Dear Name, welcome to clay solutions, Your tenant ID is TenantId. <a href='https://www.my-clay.com/'>Click here to read more about us</a></p>";
+        public static string LockAddedMsg = $"<p> Dear Name, a new lock has been added to you account. Lock Id LockId. <a href='https://www.my-clay.com/'>Click here to read more about us</a></p>";
     }
 }
