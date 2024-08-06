13eea073304b655ed684d1cca8440911bae6f0a3
DeraE2024 <careers.joseph@yahoo.com>
2024-08-06 22:00:30 +0100

    add migration - add column CreatedBy to AspNetRoles

---
+++ user-management.domain/Entities/ApplicationRole.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace user_management.domain.Entities
+{
+    public class ApplicationRole : IdentityRole
+    {
+        public string? CreatedBy { get; set; } //This should have a value if created by a tenant and null when created by our system users
+    }
+}
