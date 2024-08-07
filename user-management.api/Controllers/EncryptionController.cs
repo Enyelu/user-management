306006ea00239881f89e5027f826e9f8fb739eeb
DeraE2024 <careers.joseph@yahoo.com>
2024-08-07 20:40:59 +0100

    add encryption endpoint

---
+++ user-management.api/Controllers/EncryptionController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using user_management.core.Queries.AppEncryption;
+using user_management.core.Shared;
+
+namespace user_management.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EncryptionController : ApiBaseController
+    {
+        [HttpGet("encrypt")]
+        [AllowAnonymous]
+        [ProducesResponseType(typeof(GenericResponse<string>), 200)]
+        public async Task<IActionResult> Encrypt([FromQuery] string text)
+        {
+            var response = await Mediator.Send(new HandleEncription.Query { Text = text });
+            return Ok(response);
+        }
+    }
+}
